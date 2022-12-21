f3567c69d748a3b5d897b004539daa20460aef36
zenon <zenon@zenondeMacBook-Pro.local>
2022-12-22 01:01:10 +0800

    add

---
+++ type-challenges/3-medium-omit/template.ts
@@ -0,0 +1,18 @@
+type MyOmit<T, K extends keyof T> = {
+  [P in Exclude<keyof T, K>]: T[P]
+}
+
+// js
+const MyOmit = (T, K) => {
+  const obj = {}
+  delete T[K]
+  for (const key in T) {
+    obj[key] = T[key]
+  }
+  return obj
+}
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
