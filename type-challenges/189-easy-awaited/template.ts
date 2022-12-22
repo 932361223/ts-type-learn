1be353b51e0a3c9095387d6d398a39a1e0d344ea
zenon <zenon@zenondeMacBook-Pro.local>
2022-12-22 14:58:56 +0800

    change

--- type-challenges/189-easy-awaited/template.ts
+++ type-challenges/189-easy-awaited/template.ts
@@ -2,13 +2,21 @@
 //   ? X extends Promise<unknown>
 //     ? MyAwaited<X>
 //     : X
-//   : never
+//   :
+
 type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U>
   ? U extends PromiseLike<any>
     ? MyAwaited<U>
     : U
   : never
 
-type TRY<T> = T extends PromiseLike<infer U> ? U : never
+// 精简后的PromiseLike
+interface MyPromiseLike<T> {
+  then<TResult1 = T>(
+    onfulfilled?: (value: T) => TResult1
+  ): MyPromiseLike<TResult1>
+}
+
+type TRY<T> = T extends MyPromiseLike<infer U> ? U : never
 
 type g = TRY<{ then: (onfulfilled: (arg: number) => any) => any }>
